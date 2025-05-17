01c1b61249455cf1668a55452b4f5cd8b9fb7cbc
Leticia Furuse <leticiafurusi2005@gmail.com>
2025-05-16 21:57:48 -0300

    criando tela de formulario e tela de login

--- app/src/main/java/com/example/appmanual/pagPrincipal.kt
+++ app/src/main/java/com/example/appmanual/pagPrincipal.kt
@@ -3,21 +3,22 @@ package com.example.appmanual
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class pagPrincipal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pag_principal)
+
+
     }
 
-    fun btnEntrar(view: View){
-        var intent = Intent(this,pagPrincipal()::class.java)
+    fun btnComecar(view: View){
+        var intent = Intent(this,formulario()::class.java)
         startActivity(intent)
     }
 }
