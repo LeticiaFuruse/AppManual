90af40020ebbaf40f46b9db6625addea274c57ae
Leticia Furuse <leticiafurusi2005@gmail.com>
2025-05-17 00:41:04 -0300

    criando verificação de login(simples) -  iniciando criação de toolbar

--- app/src/main/java/com/example/appmanual/MainActivity.kt
+++ app/src/main/java/com/example/appmanual/MainActivity.kt
@@ -3,21 +3,37 @@ package com.example.appmanual
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var login: EditText
+    lateinit var senha: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login = findViewById(R.id.login)
+        senha = findViewById(R.id.senha)
     }
 
-    fun btnEntrar(view: View){
-        var intent = Intent(this,pagPrincipal()::class.java)
-        startActivity(intent)
+    fun btnEntrar(view: View) {
+        val username = login.text.toString()
+        val password = senha.text.toString()
+
+        if (verifica(username, password)) {
+            Toast.makeText(this, "Bem-vindo", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, pagPrincipal::class.java)
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Credenciais incorretas", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun verifica(username: String, password: String): Boolean {
+        return username == "admin" && password == "123"
     }
 }
