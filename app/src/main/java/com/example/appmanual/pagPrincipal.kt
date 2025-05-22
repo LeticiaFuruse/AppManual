32844c6eed84ecb8d36c76b5625f2d2f4b4df801
Leticia Furuse <leticiafurusi2005@gmail.com>
2025-05-22 18:29:46 -0300

    arrumando estilização da tela formulario, do perfil e da toolbar

--- app/src/main/java/com/example/appmanual/pagPrincipal.kt
+++ app/src/main/java/com/example/appmanual/pagPrincipal.kt
@@ -3,6 +3,7 @@ package com.example.appmanual
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -23,6 +24,18 @@ class pagPrincipal : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean{
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
+    }
+
+    // Função que captura o clique nos itens do menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.btnQuemSomos -> {
+                val intent = Intent(this, pag_quem_somos::class.java) // <-- Abre tela Quem Somos
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     fun btnComecar(view: View){
