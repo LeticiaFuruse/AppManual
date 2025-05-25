4140794fabdf8ed47b5183f1be6aa235403ad02d
Leticia Furuse <leticiafurusi2005@gmail.com>
2025-05-25 00:35:49 -0300

    adicionando btn na tabela de usuario e criando função do modal

--- app/src/main/java/com/example/appmanual/admin_usuarios.kt
+++ app/src/main/java/com/example/appmanual/admin_usuarios.kt
@@ -1,7 +1,10 @@
 package com.example.appmanual
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -12,5 +15,20 @@ class admin_usuarios : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_admin_usuarios)
 
+
     }
+
+    fun btnDelete (view:View){
+        AlertDialog.Builder(this)
+            .setTitle("Item excluido!")
+            .setMessage("você excluiu esse usuario do banco")
+            .setPositiveButton("OK"){
+                    variavel, numeroOpcao ->
+            }
+            .create()
+            .show()
+
+    }
+
+
 }
