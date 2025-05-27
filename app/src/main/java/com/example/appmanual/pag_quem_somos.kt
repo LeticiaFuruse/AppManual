e661887116e28d3dafead3b7f800ee8dbac4b99b
Leticia Furuse <leticiafurusi2005@gmail.com>
2025-05-26 22:08:00 -0300

    estilização das paginas principais e da area de admin (adicionando video a pagina Quem somos)

--- app/src/main/java/com/example/appmanual/pag_quem_somos.kt
+++ app/src/main/java/com/example/appmanual/pag_quem_somos.kt
@@ -1,11 +1,12 @@
 package com.example.appmanual
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
+import android.widget.MediaController
+import android.widget.VideoView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class pag_quem_somos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +14,23 @@ class pag_quem_somos : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_pag_quem_somos)
 
-        //chamando minha toolbar
+        // chamando minha toolbar
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        // Adicionado controle para reprodução do vídeo no VideoView
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        val videoPath = "android.resource://" + packageName + "/" + R.raw.video_teste
+        val uri = Uri.parse(videoPath)
+        videoView.setVideoURI(uri)
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        videoView.setMediaController(mediaController)
+        videoView.requestFocus()
+        videoView.start()
     }
-    //abrir o menu de botões
-    override fun onCreateOptionsMenu(menu: Menu): Boolean{
+
+    // abrir o menu de botões
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
     }
