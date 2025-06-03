02a5e79b9800fdb74100767d7716bbb9d805a052
Leticia Furuse <leticiafurusi2005@gmail.com>
2025-06-03 17:00:49 -0300

    criando lista dinamica e inserindo fontes

--- app/src/main/java/com/example/appmanual/admin_listar_produto.kt
+++ app/src/main/java/com/example/appmanual/admin_listar_produto.kt
@@ -1,6 +1,8 @@
 package com.example.appmanual
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,8 +11,19 @@ import androidx.core.view.WindowInsetsCompat
 class admin_listar_produto : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_admin_listar_produto)
+
+        val arrayAdapter: ArrayAdapter<*>
+        val produtos = arrayOf(
+            "Sabonete ant-Acne",
+            "Hidratante",
+            "Esfoliante"
+        )
+
+        var lista = findViewById<ListView>(R.id.listView)
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,produtos)
+        lista.adapter = arrayAdapter
 
     }
 }
